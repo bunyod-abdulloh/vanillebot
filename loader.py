2036409e2ac8b70944206a9fa925f0b56c10bb9f
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2026-02-06 21:01:48 +0500

    Commited

---
+++ loader.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from data import config
+from utils.db_api.admins import SendingDB
+from utils.db_api.create_tables import Database
+from utils.db_api.users import UsersDB
+
+bot = Bot(token=config.BOT_TOKEN, parse_mode=None)
+# storage = RedisStorage2(
+#     host='localhost',
+#     port=6379,
+#     db=5,
+#     state_ttl=3600,
+#     data_ttl=3600,
+# password=REDIS_PASS
+# )
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+db = Database()
+udb = UsersDB(db)
+sdb = SendingDB(db)
