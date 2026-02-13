02cb136b9b9c3790c3b9dbc7b57cf0770f3ad91a
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2026-02-13 19:27:59 +0500

    Commited

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from data import config
 from utils.db_api.admins import SendingDB
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,12 +1,12 @@
 from aiogram import Dispatcher
 
-from data.config import ADMIN_GROUP
+from data.config import ADMIN_GROUP, ADMINS
 from services.error_service import notify_exception_to_admin
 
 
 async def on_startup_notify(dp: Dispatcher):
     try:
-        await dp.bot.send_message(ADMIN_GROUP, "Bot ishga tushdi")
+        await dp.bot.send_message(ADMINS[0], "Bot ishga tushdi")
 
     except Exception as err:
         await notify_exception_to_admin(err=err)
