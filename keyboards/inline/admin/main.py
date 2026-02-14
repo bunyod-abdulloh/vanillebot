a241e502b1a12e4afe3994a155e5484f9184a28f
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2026-02-14 05:30:10 +0500

    Commited

--- keyboards/inline/admin/main.py
+++ keyboards/inline/admin/main.py
@@ -1,12 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from data.config import WEB_APP_URL
+
 
 def registration_client_ikb():
     kb = InlineKeyboardMarkup()
     kb.add(
         InlineKeyboardButton(
             text="📝 Ro'yxatdan o'tish",
-            callback_data="registration"
+            callback_data=f"{WEB_APP_URL}/anketa/"
         )
     )
     return kb
